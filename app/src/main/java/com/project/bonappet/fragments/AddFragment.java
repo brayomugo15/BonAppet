e952fb055b711ecb8d028c8cd867cfd4032356cb
Brian Mugo <brayomugo2015@gmail.com>
2021-08-02 21:34:14 +0300

    - Added Add fragment - Dimensions

---
+++ app/src/main/java/com/project/bonappet/fragments/AddFragment.java
@@ -0,0 +1,24 @@
+package com.project.bonappet.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.bonappet.R;
+
+
+public class AddFragment extends Fragment {
+
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_add, container, false);
+
+        return view;
+    }
+}
